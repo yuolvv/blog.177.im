a1a2ca664f7d90c86ef9fd952ced4efab480d925
yuolvv <yuolvv@gmail.com>
2019-12-30 10:23:36 +0800

    Update my site 2019-12-30_10:23:36

--- update_site.bat
+++ update_site.bat
@@ -6,7 +6,7 @@ echo %nowtime%
 git pull
 
 git add .
-git commit -m "Update my site %nowtime%"
+git commit -m "Update my site %date:~0,4%-%date:~5,2%-%date:~8,2%_%time:~0,2%:%time:~3,2%:%time:~6,2%"
 git push
 
 pause
